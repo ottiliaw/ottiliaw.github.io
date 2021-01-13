f1680b0a31a19b4bfaf2a0f2baaed41a0b0dc3da
ottilia <ottiliawesterlund@ottilias-mbp.home>
2021-01-13 20:12:46 +0000

    general info

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -7,8 +7,16 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="About Me" />
-    <h1>oh hello there</h1>
-    <p>there will be some fun stuff about me here (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ </p>
+    <h1>hello there (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ </h1>
+    <p>I'm Ottilia. I currently work as a web developer, and spend most of my time thinking about cyber security, art and music. </p>
+    <p>Someday I will get around to finish this webpage!</p>
+
+    <h4>Education</h4>
+    <h5>First Class B.Eng (Hons) in Computer Security and Forensics</h5>
+    <p>2014-2018 - Edinburgh Napier University <br></br>
+      My dissertation was published as a part of the 1st UAV IEEE conference in Oman 2019, and can be read <a href='https://ieeexplore.ieee.org/document/8658279'>here</a> 
+    </p>  <br></br>
+
     <Link to="/">homepage</Link>
   </Layout>
 )
--- src/pages/other-projects.js
+++ src/pages/other-projects.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="About Me" />
-    <h1>oh hello there</h1>
+    <h1>hello there</h1>
     <p>one day i will have interesting things here </p>
     <Link to="/">homepage</Link>
   </Layout>
