c5bd21d95b10bf1b3c5907fc03fe3cd4781f122d
Ottilia Westerlund <11820252+ottiliaw@users.noreply.github.com>
2021-05-28 18:05:53 +0100

    Update other-projects.js

--- src/pages/other-projects.js
+++ src/pages/other-projects.js
@@ -9,7 +9,9 @@ const SecondPage = () => (
     <SEO title="About Me" />
     <h1>hello there</h1>
     <p>one day i will have interesting things here, such as music and weird blog posts about knitting</p>
-    <p>my undergraduate dissertation was published as a part of the 1st UAV IEEE conference in Oman 2019, and can be read <a href='https://ieeexplore.ieee.org/document/8658279'>here</a> </p>
+    <h3> Publications </h3>
+    <a href='https://ieeexplore.ieee.org/document/8658279'> Drone Hacking with Raspberry-Pi 3 and WiFi Pineapple: Security and Privacy Threats for the Internet-of-Things</a>
+    <p>my undergraduate dissertation research was published as a part of the 1st UAV IEEE conference in Oman 2019</p>
   </Layout>
 )
 
